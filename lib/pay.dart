40f4e0fb2cc9a15a6ced78c64640707ccae3e4e2
MaraZ-Dev <zaras1520@gmail.com>
2023-05-26 16:47:42 +0100

    Bottomsheet

--- lib/pay.dart
+++ lib/pay.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
-class PayScreen extends StatelessWidget{
+class PayScreen extends StatelessWidget {
   PayScreen({super.key});
-  
-  
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Pay')),
+      bottomSheet: BottomSheet(
+        onClosing: () {},
+        builder: (context) {
+          return Container();
+        },
+      ),
     );
   }
 }
