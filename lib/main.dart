49b8e1e2f97e209390c96cafc7ce2341f1946cdf
Oluwatobi Togun <oluwatobitogun@gmail.com>
2023-05-29 16:43:12 +0100

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ import 'package:app/size_config.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
