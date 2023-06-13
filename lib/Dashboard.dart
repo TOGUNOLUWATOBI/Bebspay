1a5ed503408145cd6f842e2f871d7c980975db8d
Oluwatobi Togun <oluwatobitogun@gmail.com>
2023-06-13 18:42:26 +0100

    update

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -4,7 +4,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'components/BottomNavigationBar.dart';
 
 class Dashboard extends StatefulWidget {
   @override
